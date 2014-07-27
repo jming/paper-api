3613b8d6b376c4262a35bb2b8de2adaa77178eef
jming <jming@college.harvard.edu>
2014-07-27 11:09:44 +0400

    basic tracking frontend

---
+++ js/tracking.js
@@ -0,0 +1,14 @@
+$('#addTrackingFieldButton').click(function () {
+	console.log('here');
+	$('#trackingFields').append(
+		$('<div>').append(prefixSelect + prefixText)
+	);
+});
+
+var prefixSelect = '<select class="form-control form-inline">' +
+	'<option value="per">per</option>' +
+	'<option value="in">in</option>' +
+	'<option value="over">over</option>' +
+	'</select>';
+
+var prefixText = "<input type='text' class='form-control form-inline' />";
