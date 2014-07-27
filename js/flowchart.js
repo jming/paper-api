1cbd61c2eae132f2132b1647db8bf1e04a6bee5d
jming <jming@college.harvard.edu>
2014-07-27 16:35:46 +0400

    line breaks work but really really buggy

--- js/flowchart.js
+++ js/flowchart.js
@@ -1,12 +1,19 @@
-function tree(){
+var svgW=1000,
+    svgH =1000,
+    rHeight=80,
+    rWidth=120;
 
-  var svgW=1000,
-      svgH =1000,
-      rHeight=60,
-      rWidth=80,
-      tree = {x:svgW/2-rWidth/2, y:20, w:100, h:80};
+function tree() {
 
-  tree.vis={v:0, l:'START', p: {x:tree.x, y:tree.y}, c:[], t:'q'};
+  var tree = {x:svgW/2-rWidth/2, y:20, w:rWidth+10, h:rHeight+10};
+
+  tree.vis={
+    v:0,
+    l:'START',
+    p: {x:tree.x, y:tree.y},
+    c:[],
+    t:'q'
+  };
   tree.size=1;
   
   tree.getVertices =  function(){
@@ -92,9 +99,9 @@ function tree(){
       .append('rect')
         .attr('x',function(d){ return d.f.p.x;})
         .attr('y',function(d){ return d.f.p.y;})
-        .attr('height',rHeight)
+        .attr('height',function(d) { return typeProperties(d.t, 'height'); })
         .attr('width',rWidth)
-        .style('fill',function(d) {return typeColor(d.t); })
+        .style('fill',function(d) {return typeProperties(d.t, 'color'); })
       .on('click',function(d){return tree.editLeaf(d.v);})
       .transition().duration(500)
         .attr('x',function(d){ return d.p.x;})
@@ -104,20 +111,29 @@ function tree(){
       .selectAll('text')
       .data(tree.getVertices());
     
+    // deals with labels that were already there previously
     labels.text(function(d){return d.l;})
       .transition().duration(500)
-        .attr('x',function(d){ return d.p.x+10;})
-        .attr('y',function(d){ return d.p.y+20;});
+        .attr('x',function(d){ return d.p.x+5;})
+        .attr('y',function(d){ return d.p.y+10;})
+        // .call(test);
+      .call(wrap, rWidth-5);
       
+    // deals with new labels
     labels.enter()
       .append('text')
-        .attr('x',function(d){ return d.f.p.x+10;})
-        .attr('y',function(d){ return d.f.p.y+20;})
-      .text(function(d){return d.l;})
+        .attr('x',function(d){ return d.f.p.x+5;})
+        .attr('y',function(d){ return d.f.p.y+10;})
+        .text(function(d){return d.l;})
+        // .call(test)
+        // .call(wrap, rWidth-5)
         .on('click',function(d){return tree.editLeaf(d.v);})
+
+      // deals with new labels with new positions
       .transition().duration(500)
-        .attr('x',function(d){ return d.p.x+10;})
-        .attr('y',function(d){ return d.p.y+20;});
+        .attr('x',function(d){ return d.p.x+5;})
+        .attr('y',function(d){ return d.p.y+10;})
+      .call(wrap, rWidth-5);
       
   };
   
@@ -165,22 +181,27 @@ function tree(){
       .append('rect')
         .attr('x',function(d){ return d.p.x;})
         .attr('y',function(d){ return d.p.y;})
-        .attr('height',rHeight)
+        .attr('height',function(d) { return typeProperties(d.t, 'height'); })
         .attr('width',rWidth)
-        .style('fill',function(d) {return typeColor(d.t); })
+        .style('fill',function(d) {return typeProperties(d.t, 'color'); })
       .on('click',function(d){return tree.editLeaf(d.v);});
       
     d3.select("#treesvg")
       .append('g')
         .attr('id','g_labels')
         .selectAll('text')
+        // .data(tree.getVertices())
+        // .enter()
+        // .call(wrap, rWidth-10);
         .data(tree.getVertices())
         .enter()
       .append('text')
-        .attr('x',function(d){ return d.p.x;})
-        .attr('y',function(d){ return d.p.y+5;})
+        .attr('x',function(d){ return d.p.x+5;})
+        .attr('y',function(d){ return d.p.y+10;})
         .text(function(d){return d.l;})
+      // .call(wrap, rWidth)
       .on('click',function(d){return tree.editLeaf(d.v);});
+      // .call(wrap, rWidth);
   };
 
   initialize();
@@ -190,11 +211,49 @@ function tree(){
 
 var tree = tree();
 
-function typeColor(t) {
-  if (t == 's') { return '#ff9999';  } // green
-  else if (t == 'o') { return '#ffff99'; } // yellow
-  else { return '#99ff99'; } // red
+function typeProperties(t, prop) {
+  if (prop == 'color') {
+    if (t == 's') { return '#ff9999';  } // green
+    else if (t == 'o') { return '#ffff99'; } // yellow
+    else { return '#99ff99'; } // red
+  } else if (prop == 'height') {
+    return rHeight;
+    // if (t == 'q') { return rHeight; }
+    // else { return rHeight / 2; }
+  }
 }
+
+function wrap(text, width) {
+  setTimeout(function () {
+    text.each(function() {
+      var text = d3.select(this),
+        words = text.text().substring(0,240).split(/\s+/).reverse(),
+          word,
+          line = [],
+          lineNumber = 0,
+          lineHeight = 1.1, // ems
+          y = text.attr("y"),
+          x = text.attr('x'),
+          dy = 0.0,
+          tspan = text.text(null).append("tspan").attr("x", x).attr("y", y).attr("dy", dy + "em");
+      while (word = words.pop()) {
+        line.push(word);
+        tspan.text(line.join(" "));
+        if (tspan.node().getComputedTextLength() > width) {
+          line.pop();
+          tspan.text(line.join(" "));
+          line = [word];
+          tspan = text.append("tspan").attr("x", x).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+        }
+      }
+    });
+  }, 500);
+}
+
+function test (derp) {
+  console.log(derp);
+}
+
 
 $('#editLeaf-save').click(function () {
   var v = $('#editLeaf-v').val();
