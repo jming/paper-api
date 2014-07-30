610f814108066b30b390be76871e21382ee526db
jming <jming@college.harvard.edu>
2014-07-30 15:36:20 +0400

    tracking ui allows input of multiple tasks

--- js/tracking.js
+++ js/tracking.js
@@ -1,8 +1,12 @@
+// allowing for the input
 
 $('#addTrackingTaskButton').click(function () {
-  var num_tasks = $('#trackingFields div').length;
+
+  $('#createToolButton').show();
+
+  var num_tasks = $('#trackingFields .task-div').length;
   $('#trackingFields').append(
-    $('<div id="task-'+num_tasks+'">').append(
+    $('<div id="task-'+num_tasks+'" class="task-div">').append(
       $('<div id="task-'+num_tasks+'-info">').append( trackingTaskHTML )
       ).append(
       $('<div id="task-'+num_tasks+'-fields">')
@@ -11,6 +15,7 @@ $('#addTrackingTaskButton').click(function () {
         '<span class="glyphicon glyphicon-plus-sign">').append(' New field')
       )
     );
+
 });
 
 var trackingTaskHTML = '<p>I want to track the '+
@@ -32,9 +37,47 @@ var prefixSelect = '<select class="form-control form-inline">' +
 
 function addTrackingFieldTo(task_num) {
   var num_fields = $('#task-'+task_num+'-fields div').length;
-  $('#task-'+task_num+'-fields').append( $('<div>').append( prefixSelect ) );
+  $('#task-'+task_num+'-fields').append(
+    $('<div id="task-'+task_num+'-field-'+num_fields+'">').append( prefixSelect )
+  );
 }
 
-function createBasicTable () {
-  
+// creatig the table
+
+$('#createToolButton').click(function () {
+
+  // get all the information
+  var num_tasks = $('#trackingFields .task-div').length;
+  var tasks = [];
+
+  // create a basic table for each task
+  for (var i=0; i<num_tasks; i++) {
+
+    // get task info
+    var task_type = $('#task-'+i+'-info select').val();
+    var task_value = $('#task-'+i+'-info input').val();
+
+    var num_fields = $('#task-'+i+'-fields div').length;
+    var fields = [];
+
+    for (var j=0; j<num_fields; j++) {
+      var field_prefix = $('#task-'+i+'-field-'+j+' select').val();
+      var field_value = $('#task-'+i+'-field-'+j+' input').val();
+
+      fields.push([field_prefix, field_value]);
+    }
+
+    console.log(i);
+    createBasicTable(task_type, task_value, fields);
+  }
+
+  // decide how things are going to be condensed
+
+  // create the larger, more encompassing tables
+
+});
+
+function createBasicTable (type, value, fields) {
+  // create the basic table
+  console.log(type, value, fields);
 }
