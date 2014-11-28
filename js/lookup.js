afa8e7ce3691267e2f16a07b4b3d096503e8bbd3
jming <jming@college.harvard.edu>
2014-11-28 18:53:02 -0500

    calendar

--- js/lookup.js
+++ js/lookup.js
@@ -1,6 +1,6 @@
 function changeView(select) {
   var v = $(select).val();
-  var nv = (v == 'graph') ? 'year' : 'graph';
+  var nv = (v == 'graph') ? 'calendar' : 'graph';
   $('#'+v).show();
   $('#'+nv).hide();
 }
