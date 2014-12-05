a433e4eff18f05c1f94a6e4afa6b7a0ad295d455
jming <jming@college.harvard.edu>
2014-12-05 13:50:34 -0500

    resize

--- js/graph.js
+++ js/graph.js
@@ -1,5 +1,10 @@
 var global_c = new Object();
 var graph_url = "";
+var factor = 0.0;
+// var img_height = 0.0;
+
+var page_width = 225;
+var page_height = 375;
 
 function loadGraph() {
 
@@ -14,6 +19,9 @@ function loadGraph() {
 		// .append($('<img src='+graph_url+' class="graph-img-class" id="graph-img"> '));
 
 	$('#graph-img')[0].style.width = '400px';
+	// img_height = parseFloat($('#graph-img')[0].style.height.slice(0,-2));
+	// console.log(img_height);
+	// console.log($('#graph-img')[0].style.height.slice(0,-2));
 
 	// $('#graph-img')[0].style.width = '225x';
 	// $('#graph-img')[0].style.height = '330px';
@@ -37,6 +45,10 @@ function selectAxis(axis) {
 
 	// console.log(axis);
 
+	// if (img_height == 0) {
+		// img_height = parseFloat($('#graph-img')[0].style.height.slice(0,-2));
+	// }
+
 	// $('#graph-svg1-top').append('<image xlink:href='+graph_url+' height="50px" width="50px"/>');
 
 	// $('#graph-svg1-top-div').append('<image x="20" y="20" width="300" height="80" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="http://i.imgur.com/XiSVdaW.png"></image>');
@@ -46,13 +58,46 @@ function selectAxis(axis) {
 	$('#'+axis+'axis-label-text').text(label);
 
 
+	if (axis == 'c') {
+		factor = 150 / global_c.w;
+	}
+
+	console.log(factor);
+	// console.log(img_height*)
 	console.log(global_c);
 
 	$('#graph-'+axis+'axis-div').empty();
 	$('#graph-'+axis+'axis-div')
 		.append($('<img src='+graph_url+' class="graph-axis-class" id="graph-'+axis+'axis-img">'));
-	$('#graph-'+axis+'axis-img')[0].style.width = '225px';
-	$('#graph-'+axis+'axis-img')[0].style.height = '330px';
-	$('#graph-'+axis+'axis-img')[0].style.clip="rect("+global_c.y+"px, "+global_c.x2+"px, "+global_c.y2+"px, "+global_c.x+"px)";
+		// .append($('<img src='+graph_url+' id="graph-'+axis+'axis-img">'));
+	$('#graph-'+axis+'axis-img')[0].style.width = 400*factor+"px";
+	// $('#graph-'+axis+'axis-img')[0].style.height = img_height*factor+"px";
+	$('#graph-'+axis+'axis-img')[0].style.clip="rect("+global_c.y*factor+"px, "+global_c.x2*factor+"px, "+global_c.y2*factor+"px, "+global_c.x*factor+"px)";
 	// h: 56 w: 424 x: 1 x2: 425 y: 315 y2: 371
+
+	var leftm = 0;
+	var topm = 0;
+
+	if (axis == 'c') {
+		// leftm = page_width+(page_width-30-global_c.w*factor)/2;
+		leftm = 275-global_c.x*factor;
+		topm = (page_height-global_c.h*factor)/2-global_c.y*factor;
+		// $('#graph-'+axis+'axis-img')[0].style.margin-left = page_width+page_width-30+'px';
+		
+		// top right bottom left
+	}
+	if (axis == 'y') {
+		leftm = page_width + 15 - global_c.x*factor;
+		topm = (page_height-global_c.h*factor)/2-global_c.y*factor;
+
+	}
+	else if (axis == 'x') {
+		topm = page_height + 70 - global_c.y*factor;
+		// topm = 245-global_c.y*factor;
+		// leftm = (page_width*2-global_c.w*factor)/2-global_c.w*factor/2;
+		leftm = page_width - (global_c.w*factor/2) - global_c.x*factor;
+	}
+	console.log(leftm, topm); 
+	console.log(topm + "px 0px 0px" + leftm + "px");
+	$('#graph-'+axis+'axis-img')[0].style.margin = topm + "px 0px 0px " + leftm + "px";
 }
